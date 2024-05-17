fe74b70ec84e911139482f885a6a45c97e01f8e8
denis_87 <cit2270322021@mmu.ac.ke>
2024-05-17 16:00:17 +0300

    learnt about metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,16 @@
+
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    //The %s in the template will be replaced with the specific page title.Now, in your /dashboard/invoices page, you can add the page title:
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
